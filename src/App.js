18d73e035361d65b2e4b6a6858779f021ccb2ffe
DestinyJoyner <destinyjoyner@pursuit.org>
2022-10-17 11:48:26 -0400

    css for static structure of page using grid done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 function App () {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="topLevel">
+      
+      <header>
+        <h1>Bird Sanctuary Project</h1>
+      </header>
+      
+      <main>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+      </main>
+      
+      <aside>
+        <div>cart</div>
+        <div>checkout</div>
+      </aside>
+      
     </div>
   );
 };
