5beb22f733383b1dc8bf58fa7e73e2d7cd319c26
Gillesedgy <edgygilles@pursuit.org>
2022-10-17 10:36:08 -0400

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App () {
+import { useState } from "react";
+import birdData from "./data/birds";
+function App() {
+  // const [] = setState("");
   return (
     <div>
       <h1>Hello, world!</h1>
     </div>
   );
-};
+}
 
 export default App;
