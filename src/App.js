4f4583f96cc8f2f6ebece5e2bf3ee6ac0ce64eca
bowersjuan <bowersjuan@gmail.com>
2022-10-17 11:40:52 -0400

    Create static html Cart component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,22 @@ function App() {
       </div>
       <main className="main">
         <aside className="aside">
-          <div className="cart">Cart</div>
+          <div className="cart">
+            <h2>Cart</h2>
+            <h4>
+              Discount: <span>{discount}%</span>
+            </h4>
+            <h4>
+              Total: <span>${cost}</span>
+            </h4>
+            <ol>
+              <li>Adoped bird</li>
+            </ol>
+            <p>Your Donations have qualified you for the following items:</p>
+            <ol>
+              <li>First discount</li>
+            </ol>
+          </div>
           <div className="checkout">Checkout</div>
         </aside>
         <div className="birds">Birds</div>
