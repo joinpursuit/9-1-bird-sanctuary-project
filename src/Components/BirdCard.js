f1f6718d9264cb59c714919e17c69a9f1769d754
bowersjuan <bowersjuan@gmail.com>
2022-10-17 12:36:02 -0400

    Update BirdCard component

--- src/Components/BirdCard.js
+++ src/Components/BirdCard.js
@@ -3,7 +3,12 @@ export default function BirdCard({ bird }) {
     <li className="birdCard" key={bird.id}>
       <p>{bird.name}</p>
       <p>${bird.amount}</p>
-      <img src={bird.img} width={100}></img>
+      <img
+        src={bird.img}
+        height="100"
+        style={{ display: "block", margin: "auto" }}
+      ></img>
+      <button>Adopt</button>
     </li>
   );
 }
