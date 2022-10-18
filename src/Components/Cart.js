78f2265f22c1ac431673e0aed98f5b859f7f3b82
Kylie-Lang1 <kylielang@pursuit.org>
2022-10-17 21:39:22 -0400

    Added Checkout.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,7 +4,12 @@ export default function Cart ({ adoptedBirds }) {
         <aside>
             <h1>Cart</h1>
             <h2>Discount: </h2>
-            <h2>Total: $ </h2>
+            <h2>Total: ${
+                // adoptedBirds.reduce((acc, curr) => {
+                //     return acc.amount += curr.amount
+                // }, 0)
+                }
+            </h2>
             <ol>{
                 adoptedBirds.map((bird, index) => {
                     return (
--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,7 +1,24 @@
 export default function Checkout (){
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert('You have adopted birds. Thank you!')
+
+        document.querySelector(`form`).reset()
+    }
+
 return (
-    <form>
-        
+    <form onSubmit={handleSubmit}>
+        <h1>Checkout</h1>
+        <label htmlFor="first">First Name</label>
+        <input type="text" required/>
+        <label htmlFor="last">Last Name</label>
+        <input type="text" required/>
+        <label htmlFor="email">Email</label>
+        <input type="text" required />
+        <label htmlFor="zip">Zip Code</label>
+        <input type="text" required />
+        <button type="submit">Submit</button>
     </form>
 )
 }
