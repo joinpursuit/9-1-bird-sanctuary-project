f75e476d17e20667d8b9694e42eb34a4338f4123
Lennie Nurse <lnursej@gmail.com>
2022-10-18 08:54:53 -0400

    "update skeleton for bird Sanctuary"

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,7 +5,10 @@ function Cart(props) {
     <div className="Cart">
       <h3>Cart</h3>
       <h4>Total:{}</h4>
+      <span>Discount:{}</span>
       <ol></ol>
+      <p>Your donations have qualified you for the following items:</p>
+      <ul></ul>
     </div>
   )
 }
