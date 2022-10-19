7256cfa1f0ae3331cae4043c162ca983b658ab4e
bowersjuan <bowersjuan@gmail.com>
2022-10-19 01:55:06 -0400

    Update Checkout

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,8 +1,18 @@
 import "./Checkout.css";
 
 export default function Checkout() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.resetForm();
+  };
+
   return (
-    <form className="checkout">
+    <form
+      className="Checkout"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
       <h2>Checkout</h2>
       <label htmlFor="firsName">First Name:</label>
       <input type="text" id="firstName" />
