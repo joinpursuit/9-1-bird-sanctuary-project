aff225d532be40ee8c36c32a66a3d66f027536c5
minagrullon <minagrullon@pursuit.org>
2022-10-18 22:44:07 -0400

    added bonuses

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,6 +1,38 @@
 import "./Cart.css";
 
-export default function Cart({ total, discount, bonusItem, cartBirds }) {
+export default function Cart({ total, discount, bonusItems, cartBirds }) {
+  let bonuses;
+  if (total >= 1000) {
+    bonuses = (
+      <>
+        {" "}
+        <li>{bonusItems[0]}</li>
+        <li>{bonusItems[1]}</li>
+        <li>{bonusItems[2]}</li>
+        <li>{bonusItems[3]}</li>
+      </>
+    );
+  } else if (total >= 500) {
+    bonuses = (
+      <>
+        {" "}
+        <li>{bonusItems[0]}</li>
+        <li>{bonusItems[1]}</li>
+        <li>{bonusItems[2]}</li>
+      </>
+    );
+  } else if (total >= 300) {
+    bonuses = (
+      <>
+        {" "}
+        <li>{bonusItems[0]}</li>
+        <li>{bonusItems[1]}</li>
+      </>
+    );
+  } else if (total >= 100) {
+    bonuses = <li>{bonusItems[0]}</li>;
+  }
+
   return (
     <div className="Cart">
       <h2>Cart</h2>
@@ -16,7 +48,10 @@ export default function Cart({ total, discount, bonusItem, cartBirds }) {
         })}
       </ol>
       <p>Your donations have qualified you for the following items:</p>
-      <ul></ul>
+      <ul>
+        Bonus:
+        {bonuses}
+      </ul>
     </div>
   );
 }
