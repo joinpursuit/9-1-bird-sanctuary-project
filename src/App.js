9a58a53f898d610ec5004efc2b11918e4dc5d006
sreeves07 <shamarreeves@pursuit.org>
2022-10-18 23:54:00 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -2,30 +2,18 @@ import {useState} from "react";
 import birdData from "./data/birds";
 import Card from "./Components/Card";
 import Checkout from "./Components/Checkout";
-import Cart from "./Components/Cart";
+
 
 
 function App () {
 
+const [birdInfo, getBirdInfo] = useState(birdData)
 
-  // const [birdInfo, getBirdInfo] = useState(birdData)
-  // const [newEvent, setNewEvent] = useState({
-  //   firstName: "",
-  //   lastName: "",
-  //   Email: "",
-  //   ZipCode: "",
-  //   eventImage: ""
-  // });
-
-//   const birdName = birdInfo.map(name => {
-//     return (<li>{name}</li>)
-// })
 
   return (
     <div>
-      <Card />
+      <Card birdData={birdData}/>
       <Checkout />
-      <Cart />
     </div>
   );
 };
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,33 +3,52 @@ import {useState} from "react";
 import birdData from "../data/birds";
 
 
-const Card = () => {
 
-    const [birdInfo, getBirdInfo] = useState(birdData)
-    // const birdName = birdInfo.map(data => {
-    //     return (<li key={data.id}>{data.name}</li>)
-    // })
-    // const birdAmount = birdInfo.map(data => {
-    // return (<li key={data.id}>{data.amount}</li>)
-    // })
-    // const birdImg = birdInfo.map(data => {
-    //     return (<li key={data.id}>{data.img}</li>)
-    // })
-    const bird = birdInfo.map((data) => {
+
+const Card = ({birdData}) => {
+    const [total, setTotal] = useState(0)
+    const [birdsInCart, setBirdsInCart] = useState([]);
+
+    function handleSubmit(data) {
+        console.log(birdsInCart)
+        const name = data.name;
+        const amount = data.amount;
+        const id = data.id
+        if(!birdsInCart.find(e => e.name === name || e.amount === amount)) {
+            birdsInCart.push({name, amount, id});
+        }
+        setBirdsInCart(birdsInCart)
+        setTotal(total + amount)
+    }
+
+    const birdCart = birdsInCart.map((element) => <li key={element.id}>{element.name}</li>)
+
+    const bird = birdData.map((data) => {
+        // console.log(data)
         return (
             <div className='container' style={{display:"flex", margin: "30px", padding: "10px 10px", justifyContent: "space-evenly"}}>
                 <div className="card" style={{boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)", margin: "8px"}}>
                     <h3>{data.name}</h3>
                     <p>{data.amount}</p>
                     <img className="photo" src={data.img} key={data.id} style={{width:"100%"}} />
-                    <button>Adopt!</button>
+                    <button onClick={()=>{handleSubmit(data)}}>Adopt!</button>
                 </div>
             </div>
         )
     })
 
     return (
-        <div>{bird}</div>
+        <div>
+            <div>
+            <h2>Cart</h2>
+            <p>Total: ${total}</p>
+
+            <ol>{birdCart}</ol>
+           
+            <p>Discount:</p>
+            {/* {birds} */}
+        </div>
+        {bird}</div>
     )
 
 };
