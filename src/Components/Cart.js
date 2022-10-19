0eb02f31dee1a88a6fa2fa2112600843863d2905
bowersjuan <bowersjuan@gmail.com>
2022-10-19 01:47:15 -0400

    Add Bonus items rendering to Cart component

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,12 +1,31 @@
 import "./Cart.css";
 
-export default function Cart({ adoptedBirds, removeBird }) {
+export default function Cart({ adoptedBirds, removeBird, bonus }) {
   let total = adoptedBirds.reduce((acc, bird) => {
     return acc + bird.amount;
   }, 0);
 
   const totalWithRemovedBird = (bird) => {
     total -= bird.amount;
+  };
+
+  const bonusAlg = (total) => {
+    let array = [];
+
+    for (let i = 1; i <= 3; i++) {
+      if (Math.floor(i * 1.67) <= Math.floor(0.01 * total)) {
+        array = bonus.slice(0, i);
+      }
+      if (total >= 1000) {
+        array = bonus.slice(0, 4);
+      }
+    }
+
+    const res = array.map((item, idx) => {
+      return <li key={idx}>{item}</li>;
+    });
+
+    return res;
   };
 
   return (
@@ -35,9 +54,7 @@ export default function Cart({ adoptedBirds, removeBird }) {
         ))}
       </ol>
       <p>Your Donations have qualified you for the following items:</p>
-      <ol>
-        <li>First discount</li>
-      </ol>
+      <ul>{bonusAlg(total)}</ul>
     </div>
   );
 }
