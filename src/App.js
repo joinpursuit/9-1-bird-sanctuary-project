b4969a1045c854b2bb051a1af396bec96b06c8f2
shaquala <shaqualafredericks@pursuit.org>
2022-10-21 09:47:37 -0400

    project not completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import birdData from "./data/birds";
 import Cards from "./Cards"
 import Checkout from "./Checkout"
-import Cart from "./Cart"
+
 
 
 function App () {
@@ -25,8 +25,7 @@ return(
      <Checkout />
      <Cards 
      birds={bird}/>
-     
-   <Cart/>
+   
 </div>
 
  
--- src/Cards.js
+++ src/Cards.js
@@ -1,39 +1,49 @@
-import React from "react";
-import birdData from "./data/birds"
 
 
 
 
-class Cards extends React.Component{
-    render(){
- let Cards = birdData.map((birds)=> {
+ const Cards= bird.map((birds)=> {
 
-    return(
+   
 
  
-        <div className="Cards" key={birds.id}>
-            <h4>{birds.name}</h4>
-            <img src ={birds.img} alt="bird"/>
-            <p>Price: ${birds.amount}</p>
-            <button onClick={(e) => this.props.handleOnClick(birds.id)}>Adopt</button>
+//         <div className="Cards" key={birds.id}>
+//             <h4>{birds.name}</h4>
+//             <img src ={birds.img} alt="bird"/>
+//             <p>Price: ${birds.amount}</p>
+//             <button onClick={(e) => this.props.handleOnClick(birds.id)}>Adopt</button>
 
-        </div>
-        
-    )}
- )
-    
- return (
-    <div>Cards</div>
+//         </div>
+  return(
 
- )
-
+     
  
- }       
-         
-}
        
+  
+ 
+    // <div>Cards</div>
+
+ 
+      birds = ((bird) => {
+        return <h1>Cards</h1>
+    
+
+
+
+  
     
   
+     })
+    )
+  
+    
 
+
+ 
+   
+      
+     
+ 
+    })    
      
 export default Cards;
